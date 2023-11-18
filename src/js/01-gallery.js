9dc9181d5c0671016e7e3e3ae0932b2b9a53fe39
Oleh Kot <88795955+OlehKotynskyi@users.noreply.github.com>
2023-11-18 16:57:05 +0200

    new work lesson 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+
+const galleryblock = document.querySelector('.gallery');
+const itemsGallery = galleryItemsMarkup(galleryItems);
+galleryblock.insertAdjacentHTML('beforeend', itemsGallery);
+
+
+
+function galleryItemsMarkup(items) {
+   return items.map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+               <a class="gallery__link" href="${original}">
+                  <img
+                     class="gallery__image"
+                     src="${preview}"
+                     data-source="${original}"
+                     alt="${description}"
+                  />
+               </a>
+            </li>`;
+   })
+      .join('');
+}
+const simplelightbox = new SimpleLightbox('.gallery a', {
+   captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
+
+const LOCAL_INFO = 'feedback-form-state';
+
+form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
+
+let dataForm = JSON.parse(localStorage.getItem(LOCAL_INFO)) || {};
+const { email, message } = form.elements;
+reload();
+
+function onInput(e) {
+   dataForm = { email: email.value, message: message.value };
+   localStorage.setItem(LOCAL_INFO, JSON.stringify(dataForm));
+}
+
+function reload() {
+   if (dataForm) {
+      email.value = dataForm.email || '';
+      message.value = dataForm.message || '';
+   }
+}
+
+function onSubmit(e) {
+   e.preventDefault();
+   console.log({ email: email.value, message: message.value });
+
+   if (email.value === '' || message.value === '') {
+      return alert('Please fill in all the fields!');
+   }
+
+   localStorage.removeItem(LOCAL_INFO);
+   e.currentTarget.reset();
+   dataForm = {};
+}
 
